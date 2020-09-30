91f6204b3b15d9cf55f7f873eacd42e8ec84e3a7
EddyBautista <bautista.j.eddy@gmail.com>
2020-09-29 21:07:41 -0500

    added utility class

--- OOPMiniGame/Battle.cs
+++ OOPMiniGame/Battle.cs
@@ -6,5 +6,11 @@ namespace OOPMiniGame
 {
     class Battle
     {
+        // StartFight
+        // gets 2 warriors 
+        //
+        //loop giving each warrior a chance to atk and blk each turn until 1 dies
+
+        //getattkreslt
     }
 }
--- OOPMiniGame/Program.cs
+++ OOPMiniGame/Program.cs
@@ -6,7 +6,7 @@ namespace OOPMiniGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
